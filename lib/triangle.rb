be0d8c668906761a278390b8c91abdc9cf1c8b79
James Coffman <jamesc4023@gmail.com>
2022-05-18 11:21:10 -0400

    commit tree 167aec852d96aab86935c8b70aafee34d55695b5

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,48 @@
+require 'pry'
 class Triangle
   # write code here
+  attr_reader :side_one, :side_two, :side_three
+
+  def initialize(size_one, side_two, side_three)
+    @side_one = size_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+
+  def kind
+    #@kind = if(@side_one <= 0 || @side_two <= 0 || @side_two <= 0 || 
+   #           @side_one + @side_two < @side_three || @side_one + @side_three < @side_two || @side_two + @side_three < @side_one)
+   #         raise TriangleError
+   #         elsif(@side_one == @side_two && @side_one == @side_three)
+   #           :equilateral 
+   #         elsif(@side_one == @side_two || @side_one == @side_three || @side_two == @side_three)
+   #           :isosceles
+   #         else :scalene
+   #         end
+    validate_triangle
+    if(@side_one == @side_two && @side_one == @side_three)
+      :equilateral 
+    elsif(@side_one == @side_two || @side_one == @side_three || @side_two == @side_three)
+      :isosceles
+    else 
+      :scalene
+    end
 end
+
+  def sides_greater_than_zero?
+    [@side_one, @side_two, @side_three].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    @side_one + @side_two > @side_three && @side_one + @side_three > @side_two && @side_two + @side_three > @side_one
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
+end
+
+#binding.pry
